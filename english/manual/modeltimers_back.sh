4a31d6ed8fcdd2497ec30e8fa69c60e1ced99430
Lothar Thole <lothar.thole@gmail.com>
2024-12-09 16:01:58 +1030

    Tidy up keep only latest

--- english/manual/modeltimers_back.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ETHOS='/mnt/c/Program Files (x86)/FrSky/Ethos'
-
-"${ETHOS}/X20S/simulator.exe"   --read-only --audio-directory ./sd/audio/en/gb --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/model-timers.lua
-
--- english/manual/simmanro_back.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\FrSky\Ethos\X20S\simulator.exe" --read-only --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --audio-directory ./sd/audio
-
