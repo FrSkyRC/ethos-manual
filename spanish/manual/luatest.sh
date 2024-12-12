bb988b8c05212b7f21939bf3899bc4c9f6ae7ebe
Lothar Thole <lothar.thole@gmail.com>
2024-12-12 20:42:49 +1030

    Rebuild of ES images for 1.6.0

---
+++ spanish/manual/luatest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ETHOS='/mnt/c/Program Files (x86)/FrSky/Ethos'
+
+#cp models/glider/magnus.bin models
+#cp 'macro_support/lua task example' scripts
+#cp 'macro_support/source example' scripts
+#cp 'macro_support/demo source' scripts
+C:\Users\lotharthole\manual\scripts\demo source
+"${ETHOS}/X20S/simulator.exe"   --read-only --no-gui --no-audio --documents-directory ./sd/documents --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/model-lua.lua
+#rm -r 'scripts/lua task example'
+#rm -r 'scripts/source example'
+#rm -r 'scripts/demo source'
+
+
--- spanish/manual/modellua.sh
+++ spanish/manual/modellua.sh
@@ -5,7 +5,9 @@ ETHOS='/mnt/c/Program Files (x86)/FrSky/Ethos'
 # These scripts only needed for this macro
 cp -r 'macro_support/scripts/lua task example' scripts
 cp -r 'macro_support/scripts/source example' scripts
+cp -r 'macro_support/scripts/demo source' scripts
 "${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --documents-directory ./sd/documents --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/model-lua.lua
 # Remove the scripts again so the LUA icon only appears in this macro
 rm -r 'scripts/lua task example'
 rm -r 'scripts/source example'
+rm -r 'scripts/demo source'
