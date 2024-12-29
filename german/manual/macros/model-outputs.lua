2cacfcdb9c18e2cfffa302562712a313bdd4138c
Lothar Thole <lothar.thole@gmail.com>
2024-12-29 17:34:12 +1030

    Update macro for 4 way balance

--- german/manual/macros/model-outputs.lua
+++ german/manual/macros/model-outputs.lua
@@ -3,6 +3,7 @@
 -- 1 = STICK_LEFT_VERTICAL (Throttle mode 2)
 -- 2 = STICK_RIGHT_VERTICAL (Elevator)
 -- 3 = STICK_RIGHT_HORIZONTAL (Aileron)
+--2024-12-29 make 4 ch balance into ch2, ch9, ch8, ch1 instead of ch2, ch9, ch1, ch8
 
 dofile("common.lua")
 --simulator.setDateTime({year=2024, month=6, day=24, hour=20, min=0, sec=0, lock=true})
@@ -58,7 +59,7 @@ simulator.pressKey(KEY_RTN, 1)
 --start os balance section
 simulator.setAnalog(1, 40)
 simulator.pressKey(KEY_MDL)
-simulator.turnRotaryEncoder(5)
+simulator.turnRotaryEncoder(5) -- outputs
 simulator.pressKey(KEY_ENTER)
 simulator.turnRotaryEncoder(7)
 simulator.pressKey(KEY_ENTER)
@@ -86,19 +87,24 @@ simulator.pressKey(KEY_RTN)
 simulator.pressKey(KEY_ENTER)
 simulator.touch(528, 183)
 simulator.touch(526, 184)
-simulator.turnRotaryEncoder(4)
-simulator.touch(535, 334)
-simulator.turnRotaryEncoder(1)
-simulator.touch(533, 335)
-simulator.turnRotaryEncoder(2)
-simulator.touch(532, 336)
-simulator.turnRotaryEncoder(-8)
-simulator.touch(535, 135)
-simulator.turnRotaryEncoder(7)
-simulator.touch(532, 333)
+simulator.turnRotaryEncoder(4) -- to ch6
+simulator.touch(535, 334) -- untick it
+simulator.turnRotaryEncoder(1) -- to ch7
+simulator.touch(533, 335) -- untick it
+simulator.turnRotaryEncoder(2) -- to ch9
+simulator.touch(532, 336) -- tick it
+--simulator.turnRotaryEncoder(-8) -- to ch1 was ch1
+simulator.turnRotaryEncoder(-1) -- -1 ch8
+--simulator.touch(535, 135) -- tick it
+simulator.pressKey(KEY_ENTER) -- tick it
+--simulator.turnRotaryEncoder(7) -- to ch8 was ch8
+simulator.turnRotaryEncoder(-7) -- -7 ch1
+--simulator.touch(532, 333) -- tick it
+simulator.pressKey(KEY_ENTER) -- tick it
 simulator.touch(512, 386)
 simulator.touch(673, 320)
-simulator.screenshot("../assets/model-outputs-balance-ch2-9-1-8.png")
+--simulator.screenshot("../assets/model-outputs-balance-ch2-9-1-8.png")
+simulator.screenshot("../assets/model-outputs-balance-ch2-9-8-1.png")
 simulator.pressKey(KEY_RTN)
 simulator.pressKey(KEY_RTN)
 simulator.pressKey(KEY_RTN)
