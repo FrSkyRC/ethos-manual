1e373be9d25e90134cd597766a98332e0d7786a7
Lothar Thole <lothar.thole@gmail.com>
2025-01-20 16:34:17 +1030

    Update EN due to macro changes

--- german/manual/screenshots.sh
+++ german/manual/screenshots.sh
@@ -67,7 +67,7 @@ rm -r 'scripts/demo source'
 
 "${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/basic-example.lua
 
-"${ETHOS}/X20S/simulator.exe"   --read-only --no-gui --no-audio --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/wing-example.lua
+"${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/wing-example.lua
 
 "${ETHOS}/X20S/simulator.exe"   --read-only --no-gui --no-audio --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/heli-example.lua
 
