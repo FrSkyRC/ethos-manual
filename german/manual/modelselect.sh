d6d6de02010575ad0e7492cd82462c8ac08d56d3
Lothar Thole <lothar.thole@gmail.com>
2025-01-22 08:58:22 +1030

    Correct DE model select

--- german/manual/modelselect.sh
+++ german/manual/modelselect.sh
@@ -3,8 +3,8 @@
 ETHOS='/mnt/c/Program Files (x86)/FrSky/Ethos'
 
 "${ETHOS}/X20S/simulator.exe"   --read-only --no-audio --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --exec ./macros/model-select.lua
-cp models/glider/magnus.bin models
-rm -r models/airplane
-rm -r models/glider
+cp models/segler/magnus.bin models
+rm -r models/flugzeug
+rm -r models/segler
 rm -r models/heli
 rm -r models/multirotor
