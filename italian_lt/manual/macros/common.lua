ea288af8bd2ef44f3f321bcceee8d97e1f9497ab
Lothar Thole <lothar.thole@gmail.com>
2025-02-10 15:57:14 +1030

    Added Italian assets and manual folders

---
+++ italian_lt/manual/macros/common.lua
@@ -0,0 +1,1 @@
+simulator.setDateTime({year=2025, month=02, day=10, hour=20, min=0, sec=0, lock=true})
---
+++ italian_lt/manual/simman.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\FrSky\Ethos\X20S\simulator.exe" --documents-directory ./sd/documents --radio-settings ./x20s-en.bin   --sd-directory ./sd --flash-directory ./flash --audio-directory ./sd/audio 
