e7b3c77b44737f16aac51ca4d5bea3a9e225d4f4
Lothar Thole <lothar.thole@gmail.com>
2025-02-22 14:36:50 +1030

    Copy italian_lt to italian

---
+++ italian/manual/model-blanks.lua
@@ -0,0 +1,30 @@
+dofile("common.lua")
+
+-- for the blank shots
+
+simulator.loadModel("zBlank.bin")
+-- load user defined checklist
+simulator.sleep(1) -- wait for checklist to load
+simulator.screenshot("../assets/model-checklist-user-checklist.png")
+simulator.pressKey(KEY_ENTER)
+simulator.pressKey(KEY_MDL)
+
+simulator.turnRotaryEncoder(11)
+simulator.pressKey(KEY_ENTER)
+simulator.screenshot("../assets/model-lsw-add.png")
+simulator.pressKey(KEY_RTN)
+simulator.turnRotaryEncoder(1)
+simulator.pressKey(KEY_ENTER)
+simulator.screenshot("../assets/model-sf-add.png")
+simulator.pressKey(KEY_RTN)
+simulator.turnRotaryEncoder(2)
+simulator.pressKey(KEY_ENTER)
+
+
+simulator.screenshot("../assets/model-icon-vars.png")
+simulator.pressKey(KEY_ENTER)
+simulator.screenshot("../assets/model-vars-add.png")
+simulator.turnRotaryEncoder(1) -- to vars
+simulator.pressKey(KEY_ENTER)
+simulator.screenshot("../assets/model-vars-edit.png")
+simulator.pressKey(KEY_RTN, 1)
