130949f13079cca6c8c92ae3ea95a4d2de98deeb
Lothar Thole <lothar.thole@gmail.com>
2025-02-23 20:48:50 +1030

    Remove redundant italian_lt folder.

--- italian_lt/manual/simmanproaw.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\FrSky\Ethos\X20PROAW\simulator.exe" --radio-settings ./x20proaw-en.bin   --sd-directory ./sd --flash-directory ./flash --audio-directory ./sd/audio
-
--- italian_lt/manual/x18s-shots.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ETHOS='/mnt/c/Program Files (x86)/FrSky/Ethos'
-
-"${ETHOS}/X18S/simulator.exe" --read-only --no-audio --radio-settings ./x18s-en.bin --sd-directory ./sd --flash-directory ./flash --radio-directory ./sd --exec ./macros/x18s.lua
-
