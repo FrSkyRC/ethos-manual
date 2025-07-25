b7e354556d592041c18b7f306a597cee1edbd8e6
Lothar Thole <lothar.thole@gmail.com>
2025-07-25 15:35:54 +0930

    Update model-blanks for new 'add mix'

--- english/manual/macros/model-blanks.lua
+++ english/manual/macros/model-blanks.lua
@@ -43,35 +43,33 @@ simulator.turnRotaryEncoder(8) --scroll to new screen
 simulator.screenshot("../assets/display-home.png")
 simulator.pressKey(KEY_RTN, 1)
 -- get heli library screenshot
-simulator.pressKey(KEY_MDL)
-simulator.turnRotaryEncoder(1)
+simulator.pressKey(KEY_MDL) --open model menu
+simulator.turnRotaryEncoder(1) --scroll to model select
 --simulator.pressKey(KEY_ENTER)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(7)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(1)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(3)
-simulator.pressKey(KEY_ENTER)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
---simulator.turnRotaryEncoder(1)
---simulator.pressKey(KEY_ENTER)
---simulator.turnRotaryEncoder(1)
---simulator.pressKey(KEY_ENTER)
---simulator.pressKey(KEY_RTN)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_MDL)
-simulator.turnRotaryEncoder(4)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(1)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(2)
-simulator.pressKey(KEY_ENTER)
+simulator.pressKey(KEY_ENTER) --open
+simulator.turnRotaryEncoder(7) --scroll to add new model
+simulator.pressKey(KEY_ENTER) --y
+simulator.turnRotaryEncoder(1) --scroll to create model
+simulator.pressKey(KEY_ENTER) --y
+simulator.turnRotaryEncoder(3) --scroll to heli
+simulator.pressKey(KEY_ENTER) --y
+simulator.pressKey(KEY_PAGE) --accept fbl swash
+simulator.pressKey(KEY_PAGE) --accept ch reorder
+--simulator.pressKey(KEY_PAGE)
+--simulator.pressKey(KEY_PAGE)
+simulator.turnRotaryEncoder(1) --scroll to name
+simulator.pressKey(KEY_ENTER) --y
+simulator.turnRotaryEncoder(1) --scroll to A
+--simulator.pressKey(KEY_ENTER) --
+simulator.pressKey(KEY_RTN) --exit name field
+simulator.pressKey(KEY_PAGE) --page to end
+--simulator.pressKey(KEY_PAGE)
+--simulator.pressKey(KEY_PAGE)
+simulator.pressKey(KEY_MDL) --open model menu
+simulator.turnRotaryEncoder(4) --scroll to mixes
+simulator.pressKey(KEY_ENTER) --y
+simulator.turnRotaryEncoder(8) --scroll add mix
+simulator.pressKey(KEY_ENTER) --y
 simulator.screenshot("../assets/model-mixes-library-heli.png")
 simulator.pressKey(KEY_RTN, 1)
 -- get multirotor library screenshot
@@ -79,36 +77,34 @@ simulator.loadModel("zblank.bin")
 -- load user defined checklist
 simulator.sleep(1) -- wait for checklist to load
 -- simulator.screenshot("../assets/model-checklist-user-checklist.png")
-simulator.pressKey(KEY_ENTER)
+simulator.pressKey(KEY_ENTER) --ack checklist
 
-simulator.pressKey(KEY_MDL)
-simulator.turnRotaryEncoder(1)
+simulator.pressKey(KEY_MDL) --open model menu
+simulator.turnRotaryEncoder(1) --y
 --simulator.pressKey(KEY_ENTER)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(7)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(1)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(4)
-simulator.pressKey(KEY_ENTER)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
---simulator.turnRotaryEncoder(1)
---simulator.pressKey(KEY_ENTER)
---simulator.turnRotaryEncoder(1)
---simulator.pressKey(KEY_ENTER)
---simulator.pressKey(KEY_RTN)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_PAGE)
-simulator.pressKey(KEY_MDL)
-simulator.turnRotaryEncoder(4)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(1)
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(2)
-simulator.pressKey(KEY_ENTER)
+simulator.pressKey(KEY_ENTER) --open
+simulator.turnRotaryEncoder(7) --scroll to add ne model
+simulator.pressKey(KEY_ENTER) --y
+simulator.turnRotaryEncoder(1) --scroll to create model
+simulator.pressKey(KEY_ENTER) --y
+simulator.turnRotaryEncoder(4) --scroll to multirotor
+simulator.pressKey(KEY_ENTER) --y
+simulator.pressKey(KEY_PAGE) --accept ch reorder
+--simulator.pressKey(KEY_PAGE) --
+--simulator.pressKey(KEY_PAGE)
+--simulator.pressKey(KEY_PAGE)
+simulator.turnRotaryEncoder(1) --scroll to name
+simulator.pressKey(KEY_ENTER) ---
+simulator.turnRotaryEncoder(1) --scroll to A
+--simulator.pressKey(KEY_ENTER) --y
+simulator.pressKey(KEY_RTN)
+--simulator.pressKey(KEY_PAGE)
+--simulator.pressKey(KEY_PAGE)
+simulator.pressKey(KEY_PAGE) --page to end
+simulator.pressKey(KEY_MDL) --open model menu
+simulator.turnRotaryEncoder(4) --scroll to mixes
+simulator.pressKey(KEY_ENTER) --y
+simulator.turnRotaryEncoder(8) --scroll add mix
+simulator.pressKey(KEY_ENTER) --y
 simulator.screenshot("../assets/model-mixes-library-multirotor.png")
 simulator.pressKey(KEY_RTN, 1)
