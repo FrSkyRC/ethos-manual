69b6bf926a62555a80e6329458a6888e2aed3795
Lothar Thole <lothar.thole@gmail.com>
2025-08-14 15:08:02 +0930

    model-chview.lua -- adapt for no add in options, add comments

--- english/manual/macros/model-chview.lua
+++ english/manual/macros/model-chview.lua
@@ -1,5 +1,5 @@
 -- 2024-12-04 change adcValue() to setAnalog()
--- 2025-08-08 adapt macro to new Ethos 1.7 source select
+-- 2025-08-04 adapt for no add in options, add comments
 
 -- 0 = STICK_LEFT_HORIZONTAL (Rudder)
 -- 1 = STICK_LEFT_VERTICAL (Throttle mode 2)
@@ -14,30 +14,30 @@ simulator.loadModel("blaster.bin")
 simulator.setAnalog(1, -70)
 --simulator.setAnalog(ANALOG_LAST_SLIDER, (-0.0 * 100 / 1024))
 simulator.setAnalog(ANALOG_LAST_SLIDER, -1024)
-simulator.setAnalog(2, -10)
+simulator.setAnalog(2, -10) -- thr
 simulator.sleep(1) -- wait for adc values
 simulator.pressKey(KEY_ENTER) -- ack alert
 simulator.pressKey(KEY_MDL) -- open model menu
 simulator.turnRotaryEncoder(4) -- scroll to mixes
 simulator.pressKey(KEY_ENTER) -- open
-simulator.turnRotaryEncoder(4) -- scroll to ele
+simulator.turnRotaryEncoder(4) -- scroll to elevator 
 simulator.screenshot("../assets/model-mixes-chview-elevator.png")
-simulator.pressKey(KEY_ENTER)
-simulator.turnRotaryEncoder(2) -- scroll to ch view
+simulator.pressKey(KEY_ENTER) -- open mix
+simulator.turnRotaryEncoder(2) -- scroll to channel view
 simulator.screenshot("../assets/model-mixes-chview-select.png")
-simulator.pressKey(KEY_ENTER)
+simulator.pressKey(KEY_ENTER) -- enable
 simulator.sleep(3) -- wait 
 simulator.screenshot("../assets/model-mixes-chview-elevator-channel-view.png")
-simulator.turnRotaryEncoder(-1) -- scroll back to parent ch
+simulator.turnRotaryEncoder(-1) -- scroll back to ch ele parent
 simulator.screenshot("../assets/model-mixes-chview-elevator-channel.png")
-simulator.pressKey(KEY_ENTER)
+simulator.pressKey(KEY_ENTER) -- toggle to ch ele parent collapsed
 simulator.screenshot("../assets/model-mixes-chview-collapsed.png")
-simulator.pressKey(KEY_ENTER) -- uncollapse
-simulator.turnRotaryEncoder(1) -- scroll to ele
-simulator.pressKey(KEY_ENTER) -- open options menu
+simulator.pressKey(KEY_ENTER) -- toggle to ch ele parent collapsed expanded
+simulator.turnRotaryEncoder(1) -- scroll to ele mix
+simulator.pressKey(KEY_ENTER) -- open ch view options
 simulator.turnRotaryEncoder(2) -- scroll to table view
 simulator.screenshot("../assets/model-mixes-chview-table-view-select.png")
-simulator.pressKey(KEY_ENTER)
+simulator.pressKey(KEY_ENTER) -- enable table view
 simulator.screenshot("../assets/model-mixes-chview-back-at-mixes-view.png")
 simulator.pressKey(KEY_RTN, 1)
 
