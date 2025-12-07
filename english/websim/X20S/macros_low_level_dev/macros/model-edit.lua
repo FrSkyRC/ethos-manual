ed7e093d848c22fc4157b28df828f305b4cd678e
Lothar Thole <lothar.thole@gmail.com>
2025-12-07 13:35:58 +1030

    Update model-edit for Sport line removal and assets

--- english/websim/X20S/macros_low_level_dev/macros/model-edit.lua
+++ english/websim/X20S/macros_low_level_dev/macros/model-edit.lua
@@ -1,6 +1,7 @@
 -- 2025-08-14 add S.Port connector select options, add comments
 -- 2025-09-13 add model-edit-thr-deadband.png
 -- 2025-09-24 convert to web sim
+-- 2025-12-07 adapt for SPort removal in Edit Model
 
 dofile("/macros/common.lua")
 --simulator.setDateTime({year=2024, month=6, day=24, hour=20, min=0, sec=0, lock=true})
@@ -23,7 +24,7 @@ simulator.pressKey(KEY_ENTER) -- open rx typ eoptions
 simulator.screenshot("/screenshots/model-edit-receiver-type.png")
 simulator.pressKey(KEY_RTN) -- exit options
 simulator.turnRotaryEncoder(13) -- scroll to end to move page
-simulator.turnRotaryEncoder(-6) -- scroll back to thr stick deadband
+simulator.turnRotaryEncoder(-5) -- scroll back to thr stick deadband
 simulator.screenshot("/screenshots/model-edit-thr-deadband.png")
 simulator.turnRotaryEncoder(1) -- scroll to analog filter
 simulator.screenshot("/screenshots/model-edit-analog-filter.png")
@@ -35,12 +36,12 @@ simulator.screenshot("/screenshots/model-edit-fn-switches.png")
 simulator.pressKey(KEY_ENTER) -- open options
 simulator.screenshot("/screenshots/model-edit-fn-switches-select.png")
 simulator.pressKey(KEY_RTN) -- exit options
-simulator.turnRotaryEncoder(2) -- scroll to SPort conn
-simulator.screenshot("/screenshots/model-edit-sport.png")
-simulator.pressKey(KEY_ENTER) -- open SPort conn options
-simulator.screenshot("/screenshots/model-model-edit-sport-select.png")
-simulator.pressKey(KEY_RTN) -- exit options
-simulator.turnRotaryEncoder(1) -- scroll to model runtime
+--simulator.turnRotaryEncoder(2) -- scroll to SPort conn
+--simulator.screenshot("/screenshots/model-edit-sport.png")
+--simulator.pressKey(KEY_ENTER) -- open SPort conn options
+--simulator.screenshot("/screenshots/model-model-edit-sport-select.png")
+--simulator.pressKey(KEY_RTN) -- exit options
+simulator.turnRotaryEncoder(2) -- scroll to model runtime
 simulator.screenshot("/screenshots/model-edit-model-runtime.png")
 simulator.turnRotaryEncoder(1) -- scroll to reset all mixes
 simulator.screenshot("/screenshots/model-edit-model-reset_all_mixes.png")
