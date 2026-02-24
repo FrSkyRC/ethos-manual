312a24bdc320ec4361bb314b2522ca30f2f85f0e
Lothar Thole <lothar.thole@gmail.com>
2026-02-24 14:46:48 +1030

    Update frnech_LT radio_backup

---
+++ french_LT/websim/X20S/X20S_radio_backup_FR/macros/how-to-gear-seq.lua
@@ -0,0 +1,113 @@
+-- 2025-11-27 first release
+-- 2025-12-03 improve the how to with different fwd and bkwd curves
+-- 2026-02-24 remove dropped edit sequence
+
+dofile("/macros/common.lua")
+--simulator.setDateTime({year=2024, month=6, day=24, hour=20, min=0, sec=0, lock=true})
+
+--
+-- assign the doors and retracts channels
+simulator.loadModel("zblank.bin")
+simulator.pressKey(KEY_ENTER) -- ack alert
+simulator.pressKey(KEY_MDL) -- open model menu
+simulator.turnRotaryEncoder(5) -- scroll to channels
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(7) -- scroll to ch 7
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.turnRotaryEncoder(1) -- scroll to name
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.touch(393, 352) --G
+simulator.touch(62, 399) --shift
+simulator.touch(206, 308) --e
+simulator.touch(88, 355) --a
+simulator.touch(281, 304) --r
+simulator.touch(400, 450) -- space
+simulator.touch(52, 399) --shift
+simulator.touch(244, 351) --D
+simulator.touch(60, 401) -- shift
+simulator.touch(677, 307) --o
+simulator.touch(677, 307) --o
+simulator.touch(282, 308) --r
+simulator.touch(161, 354) --s
+simulator.touch(743, 450) -- enter
+simulator.screenshot("/screenshots/how-to-seq-gear-ch7.png")
+simulator.pressKey(KEY_RTN) -- deselect
+simulator.pressKey(KEY_RTN) -- exit
+simulator.turnRotaryEncoder(1) -- scroll to ch 8
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.turnRotaryEncoder(1) -- scroll to name
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.touch(280, 306) --R
+simulator.touch(70, 402) -- shift
+simulator.touch(209, 307) --e
+simulator.touch(358, 306) --t
+simulator.touch(281, 306) --r
+simulator.touch(76, 355) --a
+simulator.touch(311, 401) --c
+simulator.touch(356, 301) --t
+simulator.touch(157, 356) --s
+simulator.touch(746, 449) --enter
+simulator.screenshot("/screenshots/how-to-seq-gear-ch8.png")
+simulator.pressKey(KEY_RTN) -- deselect
+simulator.pressKey(KEY_RTN) --exit to channels
+simulator.pressKey(KEY_RTN) -- deselect
+simulator.screenshot("/screenshots/how-to-seq-gear-channels.png")
+simulator.pressKey(KEY_RTN, 0.6)
+--
+-- now do sequencer
+simulator.loadModel("signature edge.bin")
+simulator.pressKey(KEY_ENTER) -- ack alert
+simulator.pressKey(KEY_MDL) -- open model menu
+simulator.turnRotaryEncoder(4) -- scroll to mixes
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(5) -- scroll to sequence mix
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(1) -- scroll to edit
+simulator.pressKey(KEY_ENTER) -- y
+simulator.turnRotaryEncoder(1) -- scroll to name
+simulator.screenshot("/screenshots/how-to-seq-gear-name.png")
+simulator.turnRotaryEncoder(14) -- scroll to bottonm of page
+simulator.turnRotaryEncoder(-3) -- scroll back to op1 menu
+simulator.screenshot("/screenshots/how-to-seq-gear-op1-menu-button.png")
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(2) -- scroll to add bkwd curve
+simulator.screenshot("/screenshots/how-to-seq-gear-op1-add-bkwd-curve.png")
+simulator.turnRotaryEncoder(-1) -- scroll to edit fwd curve
+simulator.screenshot("/screenshots/how-to-seq-gear-op1-edit-fwd-curve.png")
+simulator.pressKey(KEY_ENTER) -- edit fwd curve
+simulator.screenshot("/screenshots/how-to-seq-gear-op1-fwd-curve.png")
+simulator.pressKey(KEY_RTN) -- exit
+simulator.turnRotaryEncoder(2) -- scroll to op2 menu
+simulator.screenshot("/screenshots/how-to-seq-gear-op2-menu-button.png")
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(1) -- scroll to edit fwd curve
+simulator.screenshot("/screenshots/how-to-seq-gear-op2-edit-fwd-curve.png")
+simulator.pressKey(KEY_ENTER)  -- edit fwd curve
+simulator.screenshot("/screenshots/how-to-seq-gear-op2-fwd-curve.png")
+simulator.pressKey(KEY_RTN) -- exit
+simulator.pressKey(KEY_ENTER) -- open menu
+simulator.turnRotaryEncoder(2) -- scroll to add backw curve
+simulator.screenshot("/screenshots/how-to-seq-gear-op2-add-bkwd-curve.png")
+simulator.pressKey(KEY_ENTER) -- add backw curve
+--simulator.pressKey(KEY_ENTER) -- open menu
+--simulator.turnRotaryEncoder(2) -- scroll edit backw curve
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.turnRotaryEncoder(2) -- scroll to pt 4
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.touch(389, 443) -- increase step
+simulator.turnRotaryEncoder(5) -- increase to 1.5s
+simulator.pressKey(KEY_ENTER) -- accept
+simulator.turnRotaryEncoder(2)
+simulator.pressKey(KEY_ENTER)
+simulator.touch(393, 449)
+simulator.turnRotaryEncoder(10)
+simulator.pressKey(KEY_ENTER)
+simulator.turnRotaryEncoder(1)
+simulator.pressKey(KEY_ENTER)
+simulator.touch(392, 450)
+simulator.turnRotaryEncoder(-20)
+simulator.pressKey(KEY_ENTER)
+simulator.turnRotaryEncoder(-5)
+simulator.screenshot("/screenshots/how-to-seq-gear-op2-bkwd-curve.png")
+simulator.pressKey(KEY_RTN)
+simulator.pressKey(KEY_RTN, 0.6)
---
+++ french_LT/websim/X20S/X20S_radio_backup_FR/macros/model-links.lua
@@ -0,0 +1,174 @@
+-- 2025-07-24 adapt macro to new Ethos 1.7 source select
+-- 2025-08-04 add comments
+-- 2025-09-04 increase wait for load to 6s
+-- 2025-12-19 reset form to blanks at start
+-- 2025-02-10 changes due to revised BT 
+-- 2025-02-16 further changes to BT for disconnect
+-- 2026-02-17 further changes from Bertrand
+
+dofile("/macros/common.lua")
+--simulator.setDateTime({year=2024, month=6, day=24, hour=20, min=0, sec=0, lock=true})
+
+simulator.loadModel("rarebear.bin")
+
+
+simulator.pressKey(KEY_ENTER) -- ackn alert
+simulator.pressKey(KEY_MDL) -- open model menu
+simulator.pressKey(KEY_PAGE) -- 2ndpage
+simulator.turnRotaryEncoder(7) -- scroll to trainer
+simulator.screenshot("/screenshots/model-icon-links.png")
+simulator.pressKey(KEY_ENTER) -- open
+-- reset links to blanks
+simulator.turnRotaryEncoder(3) -- scroll to Trainer cable
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(-2) -- scroll back to '---'
+simulator.pressKey(KEY_ENTER) -- select
+simulator.turnRotaryEncoder(-2) -- scroll back to top
+simulator.pressKey(KEY_RTN) -- deselect
+--
+simulator.screenshot("/screenshots/model-links.png")
+simulator.turnRotaryEncoder(1) -- scroll to s.port
+simulator.screenshot("/screenshots/model-links-sport-conn.png")
+simulator.pressKey(KEY_ENTER) -- open s.port
+simulator.screenshot("/screenshots/model-links-sport-conn-options.png")
+simulator.turnRotaryEncoder(1) -- scroll to fbus
+simulator.pressKey(KEY_ENTER) -- open fbus
+simulator.screenshot("/screenshots/model-links-sport-conn-fbus.png")
+simulator.pressKey(KEY_ENTER) -- exit
+simulator.turnRotaryEncoder(1) -- scroll to sbus out
+--simulator.screenshot("/screenshots/model-links-sport-conn-options-sbus-out.png")
+simulator.pressKey(KEY_ENTER) -- open sbus out
+simulator.screenshot("/screenshots/model-links-sport-conn-sbus-out.png")
+simulator.pressKey(KEY_ENTER) -- exit
+simulator.turnRotaryEncoder(1) -- scroll to sbus in 
+--simulator.screenshot("/screenshots/model-links.png")
+simulator.pressKey(KEY_ENTER) --open sbus in
+simulator.screenshot("/screenshots/model-links-sport-conn-sbus-in.png")
+-- select active cond
+simulator.turnRotaryEncoder(1) -- scroll to active cond
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.turnRotaryEncoder(3) -- scroll to sw's
+simulator.pressKey(KEY_ENTER) -- y
+simulator.turnRotaryEncoder(2) -- scroll to SA down
+simulator.pressKey(KEY_ENTER) -- y
+-- ch1
+simulator.turnRotaryEncoder(1) -- scroll to ch1
+simulator.screenshot("/screenshots/model-links-sport-conn-sbus-in-ch1-select.png")
+simulator.pressKey(KEY_ENTER) -- open ch1
+simulator.screenshot("/screenshots/model-links-sport-conn-sbus-in-ch1.png")
+simulator.turnRotaryEncoder(2)-- scroll to replace mode
+simulator.pressKey(KEY_ENTER) -- open options
+simulator.screenshot("/screenshots/model-links-sport-conn-sbus-in-mode-options.png")
+simulator.pressKey(KEY_RTN) -- exit options
+simulator.pressKey(KEY_RTN) -- deselect
+simulator.pressKey(KEY_RTN) -- back to channels
+
+simulator.turnRotaryEncoder(6) -- scroll to ch7
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.turnRotaryEncoder(4) -- scroll to destination
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.turnRotaryEncoder(3) -- scroll to sw's
+simulator.pressKey(KEY_ENTER) -- y
+simulator.turnRotaryEncoder(3) -- scroll to SD
+simulator.pressKey(KEY_ENTER) -- y
+simulator.pressKey(KEY_RTN) -- deselect
+simulator.pressKey(KEY_RTN) -- exit
+simulator.screenshot("/screenshots/model-links-bt-master-ch7-replaced.png")
+--simulator.pressKey(KEY_RTN) -- deselect
+--simulator.pressKey(KEY_RTN) -- back to ch's
+
+--simulator.pressKey(KEY_RTN) -- exit sbus in
+simulator.turnRotaryEncoder(-8) -- scroll back to sport conn
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(-3) -- scroll back to '---'
+simulator.pressKey(KEY_ENTER) -- select
+simulator.turnRotaryEncoder(2) -- scroll to trainer cable
+simulator.screenshot("/screenshots/model-links-trainer-cable.png")
+simulator.pressKey(KEY_ENTER) -- open
+simulator.screenshot("/screenshots/model-links-trainer-cable-select.png")
+simulator.turnRotaryEncoder(1) -- scroll to slave
+simulator.pressKey(KEY_ENTER) -- open slave
+simulator.screenshot("/screenshots/model-links-trainer-cable-slave.png")
+simulator.pressKey(KEY_ENTER) -- back to options
+simulator.turnRotaryEncoder(1) -- scroll master
+--simulator.screenshot("/screenshots/model-links-trainer-cable-master-select.png")
+simulator.pressKey(KEY_ENTER) -- open master
+simulator.screenshot("/screenshots/model-links-trainer-cable-master.png")
+simulator.pressKey(KEY_ENTER) -- back to options
+simulator.turnRotaryEncoder(-2) -- scroll back to '---'
+simulator.pressKey(KEY_ENTER) -- select
+simulator.turnRotaryEncoder(1) -- scroll to BT
+simulator.screenshot("/screenshots/model-links-bt.png")
+simulator.pressKey(KEY_ENTER) -- open bt
+simulator.turnRotaryEncoder(1) -- scroll to telem
+simulator.pressKey(KEY_ENTER) -- y
+simulator.screenshot("/screenshots/model-links-bt-telem.png")
+simulator.pressKey(KEY_ENTER) -- back to options
+simulator.turnRotaryEncoder(1) -- scroll to master
+simulator.screenshot("/screenshots/model-links-bt-master-select.png")
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(6) -- scroll to move master up 
+simulator.turnRotaryEncoder(-6) -- move cursor back up 
+simulator.screenshot("/screenshots/model-links-bt-master.png")
+simulator.turnRotaryEncoder(2) -- scroll to bT search
+simulator.screenshot("/screenshots/model-links-bt-master-search.png")
+simulator.pressKey(KEY_ENTER) -- search
+simulator.sleep(1) -- wait
+simulator.advertizeBluetooth("Alice", "01:01:01:01:01:01")
+simulator.advertizeBluetooth("Bob", "01:01:01:01:01:02")
+simulator.turnRotaryEncoder(1) -- scroll to Alice
+simulator.screenshot("/screenshots/model-links-bt-master-alice.png")
+simulator.pressKey(KEY_ENTER) -- select alice
+simulator.sleep(1) -- wait
+simulator.screenshot("/screenshots/model-links-bt-master-connected-ok.png")
+simulator.pressKey(KEY_ENTER) -- ack connected
+simulator.sleep(1) -- wait
+simulator.screenshot("/screenshots/model-links-bt-master-connected.png")
+simulator.pressKey(KEY_ENTER) -- open Alice
+simulator.turnRotaryEncoder(1) -- scroll to disconnect option
+simulator.screenshot("/screenshots/model-links-bt-master-disconnect-select.png")
+simulator.pressKey(KEY_RTN)
+simulator.turnRotaryEncoder(-2) -- scroll to BT
+simulator.pressKey(KEY_ENTER) -- open
+simulator.turnRotaryEncoder(1) -- scroll to slave
+simulator.screenshot("/screenshots/model-links-bt-slave-select.png")
+simulator.pressKey(KEY_ENTER) -- open
+simulator.screenshot("/screenshots/model-links-bt-slave.png")
+simulator.pressKey(KEY_ENTER) -- back to dialog
+simulator.turnRotaryEncoder(1) -- scroll to engo glasses
+simulator.screenshot("/screenshots/model-links-bt-engo-select.png")
+simulator.pressKey(KEY_ENTER)
+simulator.screenshot("/screenshots/model-links-bt-engo.png")
+simulator.pressKey(KEY_RTN, 0.6) -- home
+--
+-- now do extanel module links
+simulator.pressKey(KEY_MDL) -- open model menu
+simulator.turnRotaryEncoder(8) -- scroll to RF
+simulator.pressKey(KEY_ENTER) -- open RF
+simulator.turnRotaryEncoder(2) -- scroll to internal module
+simulator.pressKey(KEY_ENTER) -- collapse int module
+simulator.turnRotaryEncoder(1) -- scroll to external module
+simulator.pressKey(KEY_ENTER) -- open ext module
+simulator.turnRotaryEncoder(2) -- scroll to type
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.turnRotaryEncoder(4) -- scroll to end
+simulator.turnRotaryEncoder(-1) -- scroll back to trainer master ppm
+simulator.screenshot("/screenshots/model-rf-trainer-master-ppm-select.png")
+simulator.pressKey(KEY_ENTER) -- open trainer master ppm
+simulator.screenshot("/screenshots/model-rf-trainer-master-ppm.png")
+simulator.pressKey(KEY_RTN) -- deselect
+simulator.pressKey(KEY_RTN) -- back to model rf
+simulator.pressKey(KEY_ENTER) -- open rf
+simulator.turnRotaryEncoder(2) -- scroll to internal module
+simulator.pressKey(KEY_ENTER) -- collapse int module
+simulator.turnRotaryEncoder(1) -- scroll to external module
+simulator.pressKey(KEY_ENTER) -- open ext module
+simulator.turnRotaryEncoder(2) -- scroll to type
+simulator.pressKey(KEY_ENTER) -- edit
+simulator.turnRotaryEncoder(1) -- scroll back to trainer master sbus
+simulator.screenshot("/screenshots/model-rf-trainer-master-sbus-select.png")
+simulator.pressKey(KEY_ENTER) -- open trainer master sbus
+simulator.screenshot("/screenshots/model-rf-trainer-master-sbus.png")
+simulator.pressKey(KEY_RTN) -- deselect
+simulator.pressKey(KEY_RTN) -- back to model rf
+simulator.pressKey(KEY_RTN, 0.6) -- home
