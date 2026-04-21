986262eb2a0d7ae9a967a663a308d9b610abb672
Lothar Thole <lothar.thole@gmail.com>
2026-04-21 20:35:14 +0930

    Update high level macros

---
+++ english/websim/X20S/macros_high_level_dev/macros_single/main.lua
@@ -0,0 +1,7 @@
+dofile("/macros/heli-example.lua")
+dofile("/macros/how-to-low-batt.lua") -- how to 1
+dofile("/macros/how-to-consumption.lua") -- how to 3
+dofile("/macros/how-to-butterfly.lua") -- how to 6
+dofile("/macros/how-to-in-flight-comp.lua") -- how to 10
+
+dofile("/macros/how-to-gear-seq.lua") -- how to 13
