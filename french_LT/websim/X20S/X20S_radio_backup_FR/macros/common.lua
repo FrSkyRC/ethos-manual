8c63f63b197ba5743b295cfdf186fc8aac7a0ecb
bsongis-frsky <bsongis@studio1247.com>
2026-08-18 12:39:50 +0200

    Remove french_LT

--- french_LT/websim/X20S/X20S_radio_backup_FR/macros/common.lua
+++
@@ -1,7 +0,0 @@
--- 2025-01-11 add setReadOnly
---
-simulator.setDateTime({year=2025, month=9, day=29, hour=20, min=0, sec=0, lock=true})
-os.mkdir('RADIO:/screenshots')
-simulator.setReadOnly(true) -- set to read only so models do not get modified
-simulator.resetAnalogs() -- reset all analogs
-simulator.resetSwitches() -- reset all switches
