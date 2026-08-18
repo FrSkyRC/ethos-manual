75538f7f7b1aaad71c1dfbe5c7272e0bf295568a
bsongis-frsky <bsongis@studio1247.com>
2026-08-18 12:39:27 +0200

    Remove french_LT

--- french_LT/manual/simmanproawro.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\FrSky\Ethos\X20PROAW\simulator.exe" --read-only --radio-settings ./x20proaw-en.bin   --sd-directory ./sd --flash-directory ./flash --audio-directory ./sd/audio
-
