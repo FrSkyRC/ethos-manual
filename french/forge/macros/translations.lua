d582b03bca10f7503e9ca1f63ec84fb448655f24
bsongis-frsky <bsongis@studio1247.com>
2026-08-20 12:11:26 +0200

    [fr] Split the translations like it is done for EN

--- french/forge/macros/translations.lua
+++ french/forge/macros/translations.lua
@@ -1,14 +1,20 @@
 TRANSLATIONS = {
+  -- Model / Select
   Airplane = "Avion",
   Glider = "Planeur",
   Heli = "Heli",
   Multirotor = "Multirotor",
   MultirotorPosition = 420, -- position of "Multirotor" folder in Model / Select
+  -- Model / Flight Modes
   FlapsFull = "Flaps 100%",
   FlapsHalf = "Flaps 50%",
+  -- Model / Timers
   BattTimer = "Chrono Batterie",
-  TimerFilePosition = 11, -- position of "Atterissage" wav file in Model / Timer
+  Timer1FilePosition = 11, -- position of "Atterissage" wav file in Model / Timer
+  Timer2FilePosition = 11, -- position of "Atterissage" wav file in Model / Timer
   UpTest = "Chrono Gaz",
+  -- How to configure a butterfly (aka Crow) mix
+  Crowdb = "Crowdb",
   FlapOffset = "Décalage Volets",
   EleComp = "Correct. Prof."
 }
