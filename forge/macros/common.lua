ba84d55c102c5369454b082e7023b15cda32fecf
Lothar Thole <lothar.thole@gmail.com>
2026-08-29 11:16:08 +0930

    Update common.lua to add Rx VFR and ADC2 and SWR

--- forge/macros/common.lua
+++ forge/macros/common.lua
@@ -27,8 +27,11 @@ local function telemetryHook(event, line)
   if event == "line" then
     simulator.injectSPortFrame({module=0, band=0, rx=0, physId=0x98, primId=0x10, appId=0xF101, value=64}) -- RSSI on 2.4GHz
     simulator.injectSPortFrame({module=0, band=0, rx=0, physId=0x98, primId=0x10, appId=0xF010, value=0}) -- VFR on 2.4GHz -- multiplied by -1 and offset = 100   
+    simulator.injectSPortFrame({module=0, band=0, rx=0, physId=0x98, primId=0x10, appId=0xF011, value=0}) -- Rx VFR -- multiplied by -1 and offset = 100   
     simulator.injectSPortFrame({module=0, band=1, rx=0, physId=0x98, primId=0x10, appId=0xF101, value=64}) -- RSSI on 900M
+    simulator.injectSPortFrame({module=0, band=1, rx=0, physId=0x98, primId=0x10, appId=0xF103, value=10.4}) -- ADC2    
     simulator.injectSPortFrame({module=0, band=1, rx=0, physId=0x98, primId=0x10, appId=0xF104, value=201}) -- RxBatt
+    simulator.injectSPortFrame({module=0, band=1, rx=0, physId=0x98, primId=0x10, appId=0xF105, value=32}) -- SWR    
     simulator.injectSPortFrame({module=0, band=0, rx=0, physId=0x98, primId=0x10, appId=0x0300, value=cells(2, 0, 3.2, 3.7)}) -- LiPo1 (1+2)
     simulator.injectSPortFrame({module=0, band=0, rx=0, physId=0x98, primId=0x10, appId=0x0301, value=cells(6, 0, 3.69, 3.72)}) -- LiPo2 (1+2)
     simulator.injectSPortFrame({module=0, band=0, rx=0, physId=0x98, primId=0x10, appId=0x0301, value=cells(6, 2, 3.71, 3.70)}) -- LiPo2 (3+4)
